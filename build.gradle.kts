9159a1cad2eda6fe070fa31081e3d46ba3692af6
yuheesong <thddbgml345@naver.com>
2025-04-04 00:15:33 +0900

    feat: 기본 패키지 구조 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,8 +34,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.projectlombok:lombok:1.18.22")
+	implementation("org.projectlombok:lombok:1.18.22")
 
-    // DB
+	// DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
     // Test
---
+++ src/main/java/kr/hhplus/be/server/controller/balance/BalanceController.java
@@ -0,0 +1,12 @@
+package kr.hhplus.be.server.controller.balance;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v0/balance")
+@RequiredArgsConstructor
+public class BalanceController {
+
+}
